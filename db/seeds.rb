1f4a7537ed5eab4d399b807723b701cdfcec18ff
Raomir Trujillo Peña <raomir1403@gmail.com>
2020-06-01 18:53:29 -0500

    Ajustes de seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,23 @@
 Marca.create([
     { nombre: "Adidas", descripcion: "fnejhrfbwjhrb", slogan: "cwj erhbwjeh", ciudad: 'Neiva', 
       departamento: "Huila", pais: "Colombia", visible: true, color_principal: "rojo", color_secundario: "azul",
-      email: "raomir@gmail.com", radio_de_cobertura: 4, social_media: "{[facebook: 'raomir', youtube:'raomir14']}"}, 
+      email: "raomir@gmail.com", radio_de_cobertura: 4, social_media: "{[facebook: 'raomir', youtube:'raomir14']}", categorium_id: 2}, 
     
     { nombre: "Lacoste", descripcion: "fnejhrfbwjhrb", slogan: "cwj erhbwjeh", ciudad: 'Neiva', 
       departamento: "Huila", pais: "Colombia", visible: true, color_principal: "rojo", color_secundario: "azul",
-      email: "raomir@gmail.com", radio_de_cobertura: 4, social_media: "{[facebook: 'raomir', youtube:'raomir14']}"}, 
+      email: "raomir@gmail.com", radio_de_cobertura: 4, social_media: "{[facebook: 'raomir', youtube:'raomir14']}", categorium_id: 1}, 
 
     { nombre: "Samsung", descripcion: "fnejhrfbwjhrb", slogan: "cwj erhbwjeh", ciudad: 'Neiva', 
       departamento: "Huila", pais: "Colombia", visible: true, color_principal: "rojo", color_secundario: "azul",
-      email: "raomir@gmail.com", radio_de_cobertura: 4, social_media: "{[facebook: 'raomir', youtube:'raomir14']}"}, 
+      email: "raomir@gmail.com", radio_de_cobertura: 4, social_media: "{[facebook: 'raomir', youtube:'raomir14']}", categorium_id: 3}, 
     
     { nombre: "Lg", descripcion: "fnejhrfbwjhrb", slogan: "cwj erhbwjeh", ciudad: 'Neiva', 
       departamento: "Huila", pais: "Colombia", visible: true, color_principal: "rojo", color_secundario: "azul",
-      email: "raomir@gmail.com", radio_de_cobertura: 4, social_media: "{[facebook: 'raomir', youtube:'raomir14']}"}, 
+      email: "raomir@gmail.com", radio_de_cobertura: 4, social_media: "{[facebook: 'raomir', youtube:'raomir14']}" categorium_id: 4}, 
 
     { nombre: "Lacoste", descripcion: "fnejhrfbwjhrb", slogan: "cwj erhbwjeh", ciudad: 'Neiva', 
       departamento: "Huila", pais: "Colombia", visible: true, color_principal: "rojo", color_secundario: "azul",
-      email: "raomir@gmail.com", radio_de_cobertura: 4, social_media: "{[facebook: 'raomir', youtube:'raomir14']}"} 
+      email: "raomir@gmail.com", radio_de_cobertura: 4, social_media: "{[facebook: 'raomir', youtube:'raomir14']}", categorium_id: 1} 
 ])
 
 Categorium.create([
